eb94f73a2c93daf998b9b10e33d508667f3d077f
ilyastabennhas <109686507+ilyastabennhas@users.noreply.github.com>
2022-08-01 10:38:37 +0200

    updated

--- app/src/main/java/com/gitprojects_/MainActivity.kt
+++ app/src/main/java/com/gitprojects_/MainActivity.kt
@@ -2,10 +2,13 @@ package com.gitprojects_
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"kotlin to Github",Toast.LENGTH_SHORT)
     }
 }
